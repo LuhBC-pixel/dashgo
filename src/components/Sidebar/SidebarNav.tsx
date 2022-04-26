22e6edb59ec7d977a646f301385c50788c950285
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-04-26 09:23:48 -0300

    link

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -13,12 +13,16 @@ export function SidebarNav() {
   return (
     <Stack spacing='12' align='flex-start'>
       <NavSection title='GERAL'>
-        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        <NavLink icon={RiDashboardLine} href='/dashboard'>
+          Dashboard
+        </NavLink>
+        <NavLink icon={RiContactsLine} href='/users'>
+          Usuários
+        </NavLink>
       </NavSection>
       <NavSection title='AUTOMAÇÃO'>
-        <NavLink icon={RiInputMethodLine}>Formulãrios</NavLink>
-        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        <NavLink icon={RiInputMethodLine} href='/forms'>Formulãrios</NavLink>
+        <NavLink icon={RiGitMergeLine} href='/automation'>Automação</NavLink>
       </NavSection>
     </Stack>
   );
